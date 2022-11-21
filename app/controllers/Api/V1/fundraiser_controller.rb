7ee7610b78e57329e350d2d1baaa35b6bae2cd91
Szymon Biel <szymek.2k@gmail.com>
2022-11-21 18:26:49 +0100

    delete-punching-bag-change-comment-name-in-fundraiser-controller

--- app/controllers/Api/V1/fundraiser_controller.rb
+++ app/controllers/Api/V1/fundraiser_controller.rb
@@ -6,45 +6,43 @@ module Api
       before_action :authenticate_api_v1_user!, only: %i[create update]
       before_action :set_user, only: %i[update]
       before_action :set_fundraiser, only: %i[show update]
-      # before_action :set_fundraisers, only: %i[index]
       after_action { pagy_headers_merge(@pagy) if @pagy }
 
-      # GET /fundraisers
+      # GET /fundraiser
       def index
-        # @pagy, @fundraisers = pagy(Fundraiser.all)
-        @fundraisers = Fundraiser.all
+        @pagy, @fundraisers = pagy(Fundraiser.all)
 
         render json: @fundraisers, each_serializer: SimpleFundraiserSerializer
       end
 
-      # GET /fundraisers/1
+      # GET /fundraiser/1
       def show
         @fundraiser.increment
         render json: @fundraiser, serializer: FundraiserSerializer
       end
 
-      # GET /lastest
+      # GET /fundraiser/lastest
       def lastest
         @pagy, @lastest_fundraisers = pagy(Fundraiser.order(created_at: :desc))
 
         render json: @lastest_fundraisers, each_serializer: SimpleFundraiserSerializer
       end
 
-      # GET /random
+      # GET /fundraiser/random
       def random
         @pagy, @random_fundraisers = pagy(Fundraiser.order('RANDOM()'))
 
         render json: @random_fundraisers, each_serializer: SimpleFundraiserSerializer
       end
 
-      # GET /mostviewed
+      # GET /fundraiser/mostviewed
       def mostviewed
         @pagy, @most_viewed_fundraisers = pagy(Fundraiser.order(count: :desc))
 
         render json: @most_viewed_fundraisers, each_serializer: SimpleFundraiserSerializer
       end
 
-      # POST /fundraisers
+      # POST /fundraiser
       def create
         @fundraiser = current_api_v1_user.fundraisers.build(fundraiser_params)
         if @fundraiser.save
@@ -54,7 +52,7 @@ module Api
         end
       end
 
-      # PUT /fundraisers/1
+      # PUT /fundraiser/1
       def update
         if @fundraiser.update(fundraiser_params)
           render json: @fundraiser, serializer: FundraiserSerializer
